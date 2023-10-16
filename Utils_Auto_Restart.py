19305607fce22270636cfe2360bfac222dd964a8
qiliang.huang <qiliang.huang@electrifai.com>
2023-10-16 11:26:25 +0800

    Init

--- Utils_Auto_Restart.py
+++ Utils_Auto_Restart.py
@@ -14,15 +14,10 @@ logger.addHandler(console_handler)
 
 
 
-
-
-
 class InfoHub:
     def __init__(self):
         pass
 INFO_HUB = InfoHub()
-
-
 
 
 
@@ -33,11 +28,17 @@ def looper(smc_list):
     res_list = []
     while INFO_HUB.N < (len(smc_list)-1):
         try:
+
+            # Automation Start Here
+
             driver = init()
             driver.refresh()
             login(driver)
             prepare_page(driver)
             df = batch_search_smc(smc_list, driver, INFO_HUB.N)
+
+            # Automation End Here
+
             res_list.append(df)
         except Exception as e:
             driver.quit()
@@ -46,3 +47,15 @@ def looper(smc_list):
 
     df = pd.concat(res_list)
     return df
+
+
+def main():
+
+    # Fake code for demo
+    smc_list = ['', '', ...]
+
+    N = 0 #-1
+
+    # Auto restart if error
+    df = looper(smc_list, N)
+
